c96ad1da9ee40acb8c90e9f41123b319c1c4ddaf
mirlee0304 <danzzydala@gmail.com>
2023-05-02 11:10:53 +0900

    d

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,18 +3,65 @@
   상태 관리를 위해 `useState` 훅을 사용하여 할 일 목록과 입력값을 관리합니다.
   할 일 목록의 추가, 삭제, 완료 상태 변경 등의 기능을 구현하였습니다.
 */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import TodoItem from "@/components/TodoItem";
 import styles from "@/styles/TodoList.module.css";
+
+//firebase 모듈 불러오기
+import { db } from "@/firebase";
+import {
+  collection,
+  query,
+  doc,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+} from "firebase/firestore";
+
+//db의 todos 컬렉션 참조 만들기. 잘못된 컬렉션 이름 사용 방지
+const todoCollection = collection(db, "todos");
+
+
 
 // TodoList 컴포넌트를 정의합니다.
 const TodoList = () => {
   // 상태를 관리하는 useState 훅을 사용하여 할 일 목록과 입력값을 초기화합니다.
   const [todos, setTodos] = useState([]);
   const [input, setInput] = useState("");
-  const [selectedDate, setSelectedDate] = useState(null);
+  //----- const [selectedDate, setSelectedDate] = useState(null);
+
+  //+++++
+  const getTodos = async () => {
+    const q = query(todoCollection);
+    // const q = query(collection(db, "todos"), where("user", "==", user.uid));
+    // const q = query(todoCollection, orderBy("datetime", "desc"));
+
+    //firestore에서 할일 목록 조회
+    const results =await getDocs(q);
+    const newTodos = [];
+
+    //가져온 할일 목록 newTodos 에 넣기
+    results.docs.forEach((doc) => {
+      // console.log(doc.data());
+      // id 값을 Firestore 에 저장한 값으로 지정하고, 나머지 데이터를 newTodos 배열에 담기
+      newTodos.push({ id: doc.id, ...doc.data() });
+
+    });
+
+    setTodos(newTodos);
+  }
+
+  useEffect(() ==> {
+    getTodos();
+  }, []);
+
 
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
+
+  //--------
+  /*
   const addTodo = () => {
     // 입력값이 비어있는 경우 함수를 종료합니다.
     if (input.trim() === "") return;
@@ -29,16 +76,47 @@ const TodoList = () => {
     setInput("");
     setSelectedDate(null);
   };
+  */
 
+  //++++++++
+  const addTodo = () => {
+    // 입력값이 비어있는 경우 함수를 종료합니다.
+    if (input.trim() === "") return;
+    // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
+    // {
+    //   id: 할일의 고유 id,
+    //   text: 할일의 내용,
+    //   completed: 완료 여부,
+    // }
+    // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
+
+    //firestore에 추가한 할일 저장
+    const docRef = await addDoc(todoCollection, {
+      text: input,
+      completed: false,
+    });
+
+    //id 값을 firestore 에 저장한 값으로 지정
+    setTodos([...todos, [ id: docRef.id, text: input, completed: false ]]);
+    setInput("");
+  };
+
+/*
 // 할일 추가했을 때 추가했다는 alert
 function handleButtonClick() {
   // Trigger the "done" event here
-  alert("Done");
+  alert("added!");
 }
+*/
 
   // toggleTodo 함수는 체크박스를 눌러 할 일의 완료 상태를 변경하는 함수입니다.
   const toggleTodo = (id) => {
     // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
+
+
+
+    //------
+    /*
     setTodos(
       // todos.map((todo) =>
       //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
@@ -47,11 +125,31 @@ function handleButtonClick() {
       todos.map((todo) => {
         return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
       })
-    );
+    */
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          // Firestore 에서 해당 id를 가진 할 일을 찾아 완료 상태 업데이트.
+        const todoDoc = doc(todoCollection, id);
+        updateDoc(todoDoc, { completed: !todo.completed });
+        // ...todo => id: 1, text: "할일1", completed: false
+        return { ...todo, completed: !todo.completed };
+      } else {
+        return todo;
+      }  
+    });
+
+    setTodos(newTodos);
   };
 
   // deleteTodo 함수는 할 일을 목록에서 삭제하는 함수입니다.
   const deleteTodo = (id) => {
+
+    //++++
+    // Firestore 에서 해당 id를 가진 할 일을 삭제
+    const todoDoc = doc(todoCollection, id);
+    deleteDoc(todoDoc);
+
+
     // 해당 id를 가진 할 일을 제외한 나머지 목록을 새로운 상태로 저장합니다.
     // setTodos(todos.filter((todo) => todo.id !== id));
     setTodos(
@@ -60,10 +158,14 @@ function handleButtonClick() {
       })
     );
   };
+
+  //-----
+  /*
   // 할일 정렬
   const sortTodos = (todos) => {
     return todos.sort((a,b) => new Date(a.date) - new Date(b.date));
   };
+  */
 
   // 컴포넌트를 렌더링합니다.
   return (
