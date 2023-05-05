f156c61daa9f115f623c4105d78831b92bc292dc
mirlee0304 <danzzydala@gmail.com>
2023-05-05 20:58:06 +0900

    hj

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,8 @@
 */
 import React from "react";
 import styles from "@/styles/TodoList.module.css";
+import TodoList from "./TodoList";
+import { addDoc } from "firebase/firestore";
 
 // TodoItem 컴포넌트를 정의합니다.
 const TodoItem = ({ todo, onToggle, onDelete }) => {
@@ -23,6 +25,10 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
         style={{ textDecoration: todo.completed ? "line-through" : "none" }}
       >
         {todo.text}
+      </span>
+
+      <span className={styles.date}>
+      {new Date(todo.date.seconds * 1000).toLocaleString()}
       </span>
 
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -24,7 +24,6 @@ import {
 const todoCollection = collection(db, "todos");
 
 
-
 // TodoList 컴포넌트를 정의합니다.
 const TodoList = () => {
   // 상태를 관리하는 useState 훅을 사용하여 할 일 목록과 입력값을 초기화합니다.
@@ -32,11 +31,12 @@ const TodoList = () => {
   const [input, setInput] = useState("");
   //----- const [selectedDate, setSelectedDate] = useState(null);
 
+
   //+++++
   const getTodos = async () => {
-    const q = query(todoCollection);
+    //const q = query(todoCollection);
     // const q = query(collection(db, "todos"), where("user", "==", user.uid));
-    // const q = query(todoCollection, orderBy("datetime", "desc"));
+    const q = query(todoCollection, orderBy("date", "desc"));
 
     //firestore에서 할일 목록 조회
     const results = await getDocs(q);
@@ -44,7 +44,7 @@ const TodoList = () => {
 
     //가져온 할일 목록 newTodos 에 넣기
     results.docs.forEach((doc) => {
-      // console.log(doc.data());
+      //console.log(Date(todo.timestamp));
       // id 값을 Firestore 에 저장한 값으로 지정하고, 나머지 데이터를 newTodos 배열에 담기
       newTodos.push({ id: doc.id, ...doc.data() });
 
@@ -60,79 +60,39 @@ const TodoList = () => {
 
   // addTodo 함수는 입력값을 이용하여 새로운 할 일을 목록에 추가하는 함수입니다.
 
-  //--------
-  /*
-  const addTodo = () => {
-    // 입력값이 비어있는 경우 함수를 종료합니다.
-    if (input.trim() === "") return;
-    // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
-    // {
-    //   id: 할일의 고유 id,
-    //   text: 할일의 내용,
-    //   completed: 완료 여부,
-    // }
-    // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
-    setTodos([...todos, { id: Date.now(), text: input, completed: false, date: selectedDate }]);
-    setInput("");
-    setSelectedDate(null);
-  };
-  */
 
-  //++++++++
   const addTodo = async () => {
     // 입력값이 비어있는 경우 함수를 종료합니다.
     if (input.trim() === "") return;
-    // 기존 할 일 목록에 새로운 할 일을 추가하고, 입력값을 초기화합니다.
-    // {
-    //   id: 할일의 고유 id,
-    //   text: 할일의 내용,
-    //   completed: 완료 여부,
-    // }
-    // ...todos => {id: 1, text: "할일1", completed: false}, {id: 2, text: "할일2", completed: false}}, ..
 
-    //firestore에 추가한 할일 저장
+    const date = new Date();
     const docRef = await addDoc(todoCollection, {
       text: input,
       completed: false,
+      date: date,
     });
 
+
     //id 값을 firestore 에 저장한 값으로 지정
-    setTodos([...todos, { id: docRef.id, text: input, completed: false }]);
+    setTodos([...todos, { id: docRef.id, text: input, completed: false, date: date}]);
     setInput("");
+    //console.log(Date(addDoc.datet));
   };
 
-/*
-// 할일 추가했을 때 추가했다는 alert
-function handleButtonClick() {
-  // Trigger the "done" event here
-  alert("added!");
-}
-*/
 
   // toggleTodo 함수는 체크박스를 눌러 할 일의 완료 상태를 변경하는 함수입니다.
   const toggleTodo = (id) => {
     // 할 일 목록에서 해당 id를 가진 할 일의 완료 상태를 반전시킵니다.
 
-
-
-    //------
-    /*
-    setTodos(
-      // todos.map((todo) =>
-      //   todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      // )
-      // ...todo => id: 1, text: "할일1", completed: false
-      todos.map((todo) => {
-        return todo.id === id ? { ...todo, completed: !todo.completed } : todo;
-      })
-    */
+    
+  
       const newTodos = todos.map((todo) => {
         if (todo.id === id) {
           // Firestore 에서 해당 id를 가진 할 일을 찾아 완료 상태 업데이트.
         const todoDoc = doc(todoCollection, id);
-        updateDoc(todoDoc, { completed: !todo.completed });
+        updateDoc(todoDoc, { completed: !todo.completed});
         // ...todo => id: 1, text: "할일1", completed: false
-        return { ...todo, completed: !todo.completed };
+        return { ...todo, completed: !todo.completed};
       } else {
         return todo;
       }  
@@ -159,15 +119,8 @@ function handleButtonClick() {
     );
   };
 
-  //-----
-  /*
-  // 할일 정렬
-  const sortTodos = (todos) => {
-    return todos.sort((a,b) => new Date(a.date) - new Date(b.date));
-  };
-  */
 
-  // 컴포넌트를 렌더링합니다.
+ // 컴포넌트를 렌더링합니다.
   return (
     <div className={styles.container}>
       <h1 className="text-xl mb-4 font-bold underline underline-offset-4 decoration-wavy">
@@ -176,13 +129,7 @@ function handleButtonClick() {
       {/* 할 일을 입력받는 텍스트 필드입니다. */}
       <input
         type="text"
-        // className={styles.itemInput}
-        // -- itemInput CSS code --
-        // input[type="text"].itemInput {
-        //   width: 100%;
-        //   padding: 5px;
-        //   margin-bottom: 10px;
-        // }
+ 
         className="w-full p-1 mb-4 border border-gray-300 rounded"
         value={input}
         onChange={(e) => setInput(e.target.value)}
@@ -190,21 +137,7 @@ function handleButtonClick() {
       {/* 할 일을 추가하는 버튼입니다. */}
       <div className="grid">
         <button
-          // className={styles.addButton}
-          // -- addButton CSS code --
-          // button.addButton {
-          //   padding: 5px;
-          //   background-color: #0070f3;
-          //   color: white;
-          //   border: 1px solid #0070f3;
-          //   border-radius: 5px;
-          //   cursor: pointer;
-          // }
-          //
-          // button.addButton:hover {
-          //   background-color: #fff;
-          //   color: #0070f3;
-          // }
+     
           className={`w-40
                       justify-self-end
                       p-1 mb-4
@@ -215,6 +148,7 @@ function handleButtonClick() {
         >
           Add Todo
         </button>
+        
       </div>
       {/* 할 일 목록을 렌더링합니다. */}
       <ul>
